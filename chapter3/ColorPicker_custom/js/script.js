40333162a9d2a2e110e3c24d4000acf80368bd8b
MsfmWeb <masafumi.inoue9703@gmail.com>
2024-03-15 22:40:54 +0900

    サンプルコードのカスタム

---
+++ chapter3/ColorPicker_custom/js/script.js
@@ -0,0 +1,28 @@
+const text = document.querySelector('#colorText');
+const color = document.querySelector('#colorPicker');
+
+const colorBg = () => {
+    // 選択した色を背景色に設定
+    document.body.style.backgroundColor = color.value;
+    text.style.color = color.value;
+    // カラーコードを表示
+    if(color.value === '#ffffff') {
+        text.textContent = `カラーコード：${color.value}(white)`;
+    } else if(color.value === '#000000') {
+        text.textContent = `カラーコード：${color.value}(black)`;
+    }
+    else {
+        text.textContent = `カラーコード：${color.value}`;
+    }
+}
+color.addEventListener('input', colorBg);
+// 無名関数を使った書き方
+// color.addEventListener('input', () => {
+//     text.textContent = `カラーコード：${color.value}`;
+// });
+
+//パラメータを使った関数
+// const message = (weather, name) => {
+//     return `${name}さん こんにちは！今日は${weather}ですね`;
+// }
+// alert(message('晴れ', 'takashi'));
