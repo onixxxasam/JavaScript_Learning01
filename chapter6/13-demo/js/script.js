19154c11f3390c48b80b8dffc8052b3d95f8ea5c
MsfmWeb <masafumi.inoue9703@gmail.com>
2024-03-21 21:10:57 +0900

    chapter6

---
+++ chapter6/13-demo/js/script.js
@@ -0,0 +1,20 @@
+// 監視対象が範囲内に現れたら実行する動作
+const showKirin = (entries) => {
+  const keyframes = {
+    opacity: [0, 1],
+    translate: ['200px 0', 0],
+  }
+  entries[0].target.animate(keyframes, 2000);
+  // console.log(entries[0].target);
+};
+
+const options = {
+  root: document.querySelector('#scrollArea'),
+  rootMargin: '-50px',
+}
+
+// 監視ロボットの設定
+const kirinObserver = new IntersectionObserver(showKirin, options);
+
+// #kirinを監視するよう指示
+kirinObserver.observe(document.querySelector('#kirin'));
